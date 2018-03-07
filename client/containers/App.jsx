f2c695bc148b1341a723b4b09558f784e9945ced
justin.ko43 <justin.ko43@gmail.com>
2018-03-07 14:03:32 -0800

    deleted everything from app

---
+++ client/containers/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import fetch from 'cross-fetch';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
+export default App;
