989c8a7ff004cd0ec2626f3be44f9d9386a0dd50
Andrew Harris <ididrio@gmail.com>
2018-03-07 15:21:47 -0800

    fixed merge

--- client/index.js
+++ client/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App.jsx';
-<<<<<<< HEAD
 import './styles/main.css';
-=======
 import { BrowserRouter } from 'react-router';
->>>>>>> f52272707ac70685b32cd5bc90e26e6f394f25f7
 
 render(
   <App />  
