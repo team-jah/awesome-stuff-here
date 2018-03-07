df64f9eafd54ab3396a6a9574a075d9054775cc0
Andrew Harris <ididrio@gmail.com>
2018-03-07 15:47:44 -0800

    added survey button

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -3,32 +3,33 @@ import fetch from 'cross-fetch';
 import Survey from '../components/Survey';
 import Footer from '../components/Footer.jsx';
 import { Navbar, NavItem, Button } from 'react-materialize';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Landing from '../components/Landing.jsx';
 
 class App extends Component {
   render() {
     return (
-      // <BrowserRouter>      
-      //   <div className="app">
-      //     <Navbar right brand="awesome team">
-      //       <NavItem href="/companies">Companies</NavItem>
-      //       <Button waves='light'>Employers</Button>
-      //       <Button waves='light'>Candidates</Button>
-      //     </Navbar>
+      <BrowserRouter>      
+        <div className="app">
+          <Navbar right brand="awesome team">
+            <NavItem href="/companies">Companies</NavItem>
+            <Button waves='light'>Employers</Button>
+            <Button waves='light'>Candidates</Button>
+            <Link to='/survey'><Button waves='light'>Survey</Button></Link>
+          </Navbar>
 
-      //     <Switch>
-      //       <Route exact path="/" component={Landing} />
-      //       {/* <Route exact path="/user/" component={} />
-      //       <Route exact path="/matched/" component={} />
-      //       <Route exact path="/companies/" component={} />
-      //       <Route exact path="/interview/" component={} /> */}
-      //     </Switch>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/survey" component={Survey} />
+            {/* <Route exact path="/user/" component={} />
+            <Route exact path="/matched/" component={} />
+            <Route exact path="/companies/" component={} />
+            <Route exact path="/interview/" component={} /> */}
+          </Switch>
 
-      //     <Footer />
-      //   </div>
-      // </BrowserRouter>
-      <Survey />
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
