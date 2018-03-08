ffe6117a3731162cc6a651ea5245edb850e5ade9
justin.ko43 <justin.ko43@gmail.com>
2018-03-07 16:53:25 -0800

    landing page sorta styled

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { Button } from 'react-materialize';
 
 const Landing = () => (
   <div>
-    Hello
+    <div className="page-bg"></div>
+    <div id="landing" className="row">
+      <div className="col-md-6">
+        <h2>Introducing Awesome Team</h2>
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt dapibus aliquam. 
+          Cras porta lacus justo, sit amet fringilla elit accumsan non. Duis sit amet maximus erat. 
+          Quisque mollis ultrices justo sed mattis. Cras vehicula arcu nulla, quis condimentum turpis mattis vel. 
+        </h4>
+        <div>
+          <Button waves='light'>Candidates</Button>
+          <Button waves='light'>Employers</Button>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
