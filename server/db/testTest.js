9459e1c6b1af3b58bc98abfe4539a6b1de225e6e
Henry <henryau01@gmail.com>
2018-03-07 18:05:56 -0800

    Company db stuff & testTest

---
+++ server/db/testTest.js
@@ -0,0 +1,182 @@
+/*
+  1. Which of the following is true about variable naming conventions in JavaScript?
+    A - JavaScript variable names must begin with a letter or the underscore character.
+    B - JavaScript variable names are case sensitive.
+    C - Both of the above.
+    D - None of the above.
+
+    C
+
+  2. Which of the following is the correct syntax to redirect a url using JavaScript?
+    A - document.location='http://www.newlocation.com';
+    B - browser.location='http://www.newlocation.com';
+    C - navigator.location='http://www.newlocation.com';
+    D - window.location='http://www.newlocation.com';
+
+    D
+
+  3. Which of the following is correct about callbacks?
+    A - A callback is a plain JavaScript function passed to some method as an argument or option.
+    B - Some callbacks are just events, called to give the user a chance to react when a certain state is triggered.
+    C - Both of the above.
+    D - None of the above.
+
+    C
+
+  4. Which of the following function of Number object formats a number with a specific number of digits to the right of the decimal?
+    A - toExponential()
+    B - toFixed()
+    C - toPrecision()
+    D - toLocaleString()
+
+    B
+
+  5. Which of the following function of Number object defines how many total digits to display of a number?
+    A - toExponential()
+    B - toFixed()
+    C - toLocaleString()
+    D - toPrecision()
+
+  6. Which of the following function of String object is used to find a match between a regular expression and a string, and to replace the matched substring with a new substring?
+    A - concat()
+    B - match()
+    C - replace()
+    D - search()
+
+    C
+
+  7. Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag?
+    A - anchor()
+    B - big()
+    C - blink()
+    D - bold()
+
+    D
+
+  8. Which of the following function of String object causes a string to be italic, as if it were in an <i> tag?
+    A - fixed()
+    B - fontcolor()
+    C - fontsize()
+    D - italics()
+
+    D
+
+  9. Which of the following function of Array object joins all elements of an array into a string?
+    A - concat()
+    B - join()
+    C - pop()
+    D - map()
+
+    B
+
+  10. Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?
+    A - unshift()
+    B - sort()
+    C - splice()
+    D - toString()
+
+    A
+*/
+
+module.exports = [
+  {
+    question: '1. Which of the following is true about variable naming conventions in JavaScript?',
+    choices: [
+      'A - JavaScript variable names must begin with a letter or the underscore character.',
+      'B - JavaScript variable names are case sensitive.',
+      'C - Both of the above.',
+      'D - None of the above.',
+    ],
+    answer: 2
+  },
+  {
+    question: '2. Which of the following is the correct syntax to redirect a url using JavaScript?',
+    choices: [
+      'A - document.location=\'http://www.newlocation.com\';',
+      'B - browser.location=\'http://www.newlocation.com\';',
+      'C - navigator.location=\'http://www.newlocation.com\';',
+      'D - window.location=\'http://www.newlocation.com\';',
+    ],
+    answer: 3
+  },
+  {
+    question: '3. Which of the following is correct about callbacks?',
+    choices: [
+      'A - A callback is a plain JavaScript function passed to some method as an argument or option.',
+      'B - Some callbacks are just events, called to give the user a chance to react when a certain state is triggered.',
+      'C - Both of the above.',
+      'D - None of the above.',
+    ],
+    answer: 2
+  },
+  {
+    question: '4. Which of the following Number object function formats a number with a specific number of digits to the right of the decimal?',
+    choices: [
+      'A - toExponential()',
+      'B - toFixed()',
+      'C - toPrecision()',
+      'D - toLocaleString()',
+    ],
+    answer: 1
+  },
+  {
+    question: '5. Which of the following Number object function defines how many total digits of a number to display?',
+    choices: [
+      'A - toExponential()',
+      'B - toFixed()',
+      'C - toLocaleString()',
+      'D - toPrecision()',
+    ],
+    answer: 3
+  },
+  {
+    question: '6. Which of the following String object function is used to find a match between a regular expression and a string, and to replace the matched substring with a new substring?',
+    choices: [
+      'A - concat()',
+      'B - match()',
+      'C - replace()',
+      'D - search()',
+    ],
+    answer: 2
+  },
+  {
+    question: '7. Which of the following String object function creates a string to be displayed as bold as if it were in a <b> tag?',
+    choices: [
+      'A - anchor()',
+      'B - big()',
+      'C - blink()',
+      'D - bold()',
+    ],
+    answer: 3
+  },
+  {
+    question: '8. Which of the following String object function causes a string to be italic, as if it were in an <i> tag?',
+    choices: [
+      'A - fixed()',
+      'B - fontcolor()',
+      'C - fontsize()',
+      'D - italics()',
+    ],
+    answer: 3
+  },
+  {
+    question: '9. Which of the following Array object function joins all elements of an array into a string?',
+    choices: [
+      'A - concat()',
+      'B - join()',
+      'C - pop()',
+      'D - map()',
+    ],
+    answer: 1
+  },
+  {
+    question: '10. Which of the following Array object function adds one or more elements to the front of an array and returns the new length of the array?',
+    choices: [
+      'A - unshift()',
+      'B - sort()',
+      'C - splice()',
+      'D - toString()',
+    ],
+    answer: 0
+  }
+];
